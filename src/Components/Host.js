61c870df7ed3b77c975dc1d9b633101764fba32f
Jhaycee <johnllaneta05@gmail.com>
2022-04-22 01:51:05 +0800

    update host

--- src/Components/Host.js
+++ src/Components/Host.js
@@ -1,5 +1,5 @@
-// const hostUrl = "https://terravet-server.herokuapp.com";
-const hostUrl = "http://localhost:3001";
+const hostUrl = "https://terravet-server.herokuapp.com";
+// const hostUrl = "http://localhost:3001";
 
 const newsAPI = "3e311781a1794a6ca438e0921658af66";
 export { hostUrl, newsAPI };
